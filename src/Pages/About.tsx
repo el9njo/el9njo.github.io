476b78b970bfe753764dc97d489c3cdf9e5add10
Eirik <eirioy@stud.ntnu.no>
2025-12-30 22:59:01 +0100

    Setup for routes using react router

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -4,7 +4,7 @@ export function About() {
     return (
         <>
             <div>
-                <h1 className="text-teal-600 text-3xl p-4">Eirik Øy</h1>
+                <h1 className="text-teal-600 text-3xl p-4">Eirik</h1>
 
                 <p className="text-2xl text-teal-600"> This is a work in progress and will be continously developed</p>
             </div>
