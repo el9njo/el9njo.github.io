1cf20af38ced9c150a6346a997dd5e3323d0dfaf
El9njo <eirikoey@gmail.com>
2025-12-30 23:07:15 +0100

    Setup for routes using react router

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -7,6 +7,7 @@ export function About() {
                 <h1 className="text-teal-600 text-3xl p-4">Eirik</h1>
 
                 <p className="text-2xl text-teal-600"> This is a work in progress and will be continously developed</p>
+                <p className="bg-amber-400">Testing</p>
             </div>
         </>
     )
