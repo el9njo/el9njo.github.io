b0a81f56fb3d2888b42eb3faa7f29588c8150aec
El9njo <eirikoey@gmail.com>
2026-03-31 22:00:30 +0200

    Started for About me page

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -6,8 +6,10 @@ export function About() {
             <div>
                 <h1 className="text-teal-600 text-3xl p-4">About me</h1>
 
-                <img src="https://vercel.app"
-                     alt="GitHub Statistikk for Ola Nordmann" />
+                <img
+                    src="https://github-readme-stats.vercel.app/api?username=el9njo&show_icons=true"
+                    alt="GitHub stats"
+                />
 
             </div>
         </>
