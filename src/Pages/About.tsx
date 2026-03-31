77378484fe5826b1361be0e7fbd496f75ea9e470
El9njo <eirikoey@gmail.com>
2026-03-31 22:15:16 +0200

    Started for About me page

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -7,11 +7,7 @@ export function About() {
                 <h1 className="text-teal-600 text-3xl p-4">About me</h1>
 
                 <img
-                    src="https://github-stats-card-generator.vercel.app/api/svg?username=el9njo&theme=dracula"
-                    alt="GitHub stats"
-                />
-                <img
-                    src="https://github-stats-card-generator.vercel.app/api/svg?username=el9njo&type=contributions&theme=dracula"
+                    src="https://github-stats-card-generator.vercel.app/api/svg?username=el9njo&type=languages"
                     alt="GitHub stats"
                 />
 
